89ff36f2503b7689425f5e59156597e2a9d3625b
Duany Pena <94643825+penaduany@users.noreply.github.com>
2021-12-10 00:20:04 +0000

    first, i defined num as the input and numsquares as the number of squares. Next, i wrote program so that it prints the number of squares less than the inputted number

--- part1.py
+++ part1.py
@@ -8,13 +8,11 @@ The program below is supposed to find and print the number of perfect squares th
 Example of what should appear on the console when this part runs:
 
 Enter a number: 50
-Number of squares less than or equal to 50: 7
+Number of squares less than or equal to 50: 8
 
 '''
-num = int(input("Enter a number:"))
-numbertosquare = 1
+num = int(input("Enter a number: "))
 numsquares = 0
-while numbertosquare ** 2 <= num:
-    numsquares = numsquares + 1
-    
-print("Number of squares less than or equal to", str(num)+':', numsquares)
+while numsquares ** 2 <= num:
+  numsquares = numsquares + 1
+print("Number of squares less than or equal to:", numsquares)
